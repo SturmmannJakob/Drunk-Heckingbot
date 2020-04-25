506e251c39b5082cbf576d0a8bbc796b978f6a77
SturmmannJakob <35278125+SturmmannJakob@users.noreply.github.com>
2020-04-24 22:01:07 -0400

    Update beers.ts

--- src/commands/beers.ts
+++ src/commands/beers.ts
@@ -8,7 +8,7 @@ export class Beers extends Command {
       group: 'basic-commands',
       aliases: ['beer'],
       memberName: 'beers',
-      description: `Add the beer you're enjoying to Drunkcord!`,
+      description: `Tells you about the beer you're enjoying to Drunkcord!`,
       examples: ['!beers'],
       args: [
         {
@@ -26,7 +26,6 @@ export class Beers extends Command {
     try {
       const data = await getBeerInformation(beerName);
       const richBeerName = data.beer_name.replace(/'/g, '');
-      await addDrink(message.author.username, message.guild.id, richBeerName);
       const fancyBeerMessage = new RichEmbed()
         .setAuthor(`It looks like you're drinking a ${richBeerName}!`)
         .setTitle('Let me tell you about that beer!')
@@ -42,20 +41,6 @@ export class Beers extends Command {
       message.channel.send(fancyBeerMessage);
     } catch (error) {
       console.error('An error occurred while adding a drink', error);
-      try {
-        await addDrink(message.author.username, message.guild.id, beerName.replace(/'/g, ''));
-        const embed = new RichEmbed()
-          .setTitle('Oh no!')
-          .setColor(0xff0000)
-          .setThumbnail('https://i.imgur.com/gaf3cVL.png')
-          .setDescription(
-            `I can't find any information about that beer! I'm so sorry to have let you down :(.)`
-          );
-        return message.say(embed);
-      } catch (error) {
-        console.log('Error adding drink:', error);
-        throw new Error(`An error occurred while adding this drink :(.`);
-      }
     }
   }
 }
